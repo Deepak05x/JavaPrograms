141394aac50468beafbe25f8cbae308425d92d1a
Deepak <hdeepak5902@gmail.com>
2024-10-04 11:25:06 +0530

    Bus

---
+++ Bus.java
@@ -0,0 +1,12 @@
+public class Bus {
+    private int busno;
+    private boolean ac;
+    private int cap;
+
+    public Bus(int busno, boolean ac, int cap) {
+        this.busno = busno;
+        this.ac = ac;
+        this.cap = cap;
+    }
+
+}
