bc68799ef4d81bbf913cc0482c023a9bda5983e8
Deepak <hdeepak5902@gmail.com>
2024-10-07 10:14:14 +0530

    Similar Subarray

--- SimilarSubarray.java
+++ SimilarSubarray.java
@@ -1,15 +1,32 @@
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SimilarSubarray {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[] arr = new int[n];
+        HashSet<Integer> set = new HashSet<>();
         for (int i = 0; i < arr.length; i++) {
             arr[i] = in.nextInt();
+            set.add(arr[i]);
         }
-        System.out.println(Arrays.toString(arr));
+
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i : set) {
+            List<Integer> sub = new ArrayList<>();
+            for (int j : arr) {
+                if (j == i) {
+                    sub.add(j);
+                }
+            }
+            list.add(sub);
+        }
+
+        System.out.println(list);
 
     }
 }
