fc4b6a79af73f83c9eab9b49fc9545fce2c94128
Deepak05X <hdeepak5902@gmail.com>
2025-07-12 18:56:56 +0530

    Hallow_Diamond

---
+++ Diamond.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Diamond {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        Diamond obj1 = new Diamond();
+        obj1.upperTriangle(x);
+        obj1.lowerTriangle(x);
+
+    }
+
+    public void upperTriangle(int x){
+        for(int i = 1 ; i<= x ; i++){
+            for(int s = 1 ; s<=x - i ; s++){
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void lowerTriangle(int x){
+        for(int i = x; i>= 0; i--){
+            for(int k = 0 ; k < x - i ; k++){
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
